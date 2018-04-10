bd6bc1b87480610d888ce3356ed524f735dbeacf
Mike-Heneghan <m.heneghan@live.co.uk>
2018-04-10 18:05:59 +0100

    Test for addBook condition written and fails as designed. Need to add conditonal statement to addBook.

---
+++ src/main/java/Library.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Library {
+    private ArrayList<Book> catalogue;
+    private int capacity;
+
+    public Library(int capacity){
+        this.catalogue = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+    public int getBookCount(){
+        return this.catalogue.size();
+    }
+
+    public void addBook(Book book) {
+        catalogue.add(book);
+    }
+
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+    private Library emptyLibrary;
+    private Library smallLibrary;
+    private Library tinyLibrary;
+    private Book book1;
+    private Book book2;
+
+    @Before
+    public void before() {
+        emptyLibrary = new Library(50);
+        smallLibrary = new Library(5);
+        tinyLibrary = new Library(1);
+        book1 = new Book();
+        book2 = new Book();
+    }
+
+    @Test
+    public void hasEmptyCatalogue(){
+        assertEquals(0,emptyLibrary.getBookCount());
+    }
+
+    @Test
+    public void canAddBook(){
+        smallLibrary.addBook(book1);
+        assertEquals(1, smallLibrary.getBookCount());
+    }
+
+    @Test
+    public void wontaddBook(){
+        tinyLibrary.addBook(book1);
+        tinyLibrary.addBook(book2);
+        assertEquals(1, tinyLibrary.getBookCount());
+    }
+
+
+}
+
+
